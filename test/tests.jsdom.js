53682ba2b512fb09e5aa29f2ff28a2a615a53c04
Dominykas Blyžė <hello@dominykas.com>
2015-11-23 00:49:11 +0200

    bump jsdom; jsdom-compat for pre-LTS, jsdom@latest for post-LTS

--- test/tests.jsdom.js
+++ test/tests.jsdom.js
@@ -1,16 +1,20 @@
 global.buster = require("buster");
 var createTests = require("./tests");
 var HTML5Outline = require("../dist/outliner.min");
-var contextPath = require("path").resolve(__dirname + "/..");
+var path = require("path");
+var contextPath = "file://" + path.resolve(__dirname + "/..");
 
 function runTestsIn(jsdomModule) {
 	require(jsdomModule).env(
-		"<div></div>",
-		{features: {FetchExternalResources: ["iframe"]}},
-		function (e, w) {
-			createTests(jsdomModule, w.document, contextPath, HTML5Outline);
-		});
+			"<div></div>",
+			{features: {FetchExternalResources: ["iframe"]}},
+			function (e, w) {
+				createTests(jsdomModule, w.document, contextPath, HTML5Outline);
+			});
 }
 
-runTestsIn("jsdom-compat");
-!process.version.indexOf("v0.") || runTestsIn("jsdom");
+if (process.version.indexOf("v0.") >= 0) {
+	runTestsIn("jsdom-compat");
+} else {
+	runTestsIn("jsdom");
+}
